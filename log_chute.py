00b251e03c5193d989b4a19abb0dba00d32c21f1
Trent Scheffert <trent.scheffert@gmail.com>
2017-07-17 19:48:20 -0500

    Remove duplicate iterations and objects

--- log_chute.py
+++ log_chute.py
@@ -29,24 +29,24 @@ def parse(filename):
 
     # Scan through a string, looking for any location where this RE matches.
     match = re.compile(log_re).match
-    lines = file(filename).readlines()
-    print len(lines)
-    for line in lines:
-        print match(line).groupdict()
     # Bets on if we'll have memory issues?
     matches = (match(line) for line in file(filename).readlines())
-    match_tuples = (match.groupdict() for match in matches if match)
+    matched_tuples = (match.groupdict() for match in matches if match)
 
-    total = sum(1 for x in match_tuples)
+    total = 0
+    for matched in matched_tuples:
+        print matched
+        total = total + 1
+
     print "Found matching lines: ", total
-    return match_tuples
+    return matched_tuples
 
 def main():
     if len(sys.argv) < 2:
         print "You didn't pass a filename to parse!"
         sys.exit(1)
     filename = sys.argv[1]
-    print parse(filename)
+    parse(filename)
 
 if __name__ == '__main__':
     main()
