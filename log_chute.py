1a1b02a02a8f4f340415cced917fbda94a578e5d
Trent Scheffert <trent.scheffert@gmail.com>
2017-07-17 19:10:07 -0500

    Test that each regex matching group works on sample

--- log_chute.py
+++ log_chute.py
@@ -19,39 +19,40 @@ def parse(filename):
             'referral':x.group('referral'),
             'agent':x.group('agent'),
             }
-    log_re = re.compile(r"""
-        (?P<ip>[.:0-9a-fA-F]{7,45})
-        \s
-        (?P<identd>.*)
-        \s
-        -
-        \s
-        \[(?P<time>.*?)\]
-        \s
-        "GET (?P<uri>.*?) HTTP/1.\d"
-        \s
-        (?P<status_code>\d+)
-        \s
-        \d+
-        \s
-        "(?P<referral>.*?)"
-        \s
-        "(?P<agent>.*?)"
-        \s
-        "(?P<response_time>\d+?)"
-    """, re.VERBOSE)
     # Scan through a string, looking for any location where this RE matches.
     search = re.compile(log_re).search
     matches = (search(line) for line in file(filename))
     return (make_entry(x) for x in matches if x)
 
-matchers = {
-        'ip':'(?P<ip>[.:0-9a-fA-F]{7,45})'
-        }
+def run_matcher(match_group):
+    log_re = re.compile(r"""
+        \A
+        (?P<ip>[.:0-9a-fA-F]{7,45})
+        \s
+        (?P<identd>-|.+?)
+        \s
+        (?P<userid>-|.+?)
+        \s
+        \[(?P<timestamp>.+?)\]
+        \s
+        "(?P<uri>[A-Z]+?\ /.*?\ HTTP/1.\d)"
+        \s
+        (?P<status_code>\d+?)
+        \s
+        (?P<size>\d+?)
+        \s
+        "(?P<referer>.*?)"
+        \s
+        "(?P<user_agent>.*?)"
+        \s
+        (?P<duration_microseconds>\d+?)
+        \Z
+    """, re.VERBOSE)
 
-def run_matcher(matcher, group):
     line = '134.178.48.89 - - [21/Apr/2013:03:16:31 -0500] "DELETE /kms/alert/form/ HTTP/1.0" 200 5110 "https://example.com/kms/ledes/import/" "Mozilla/5.0 (Windows NT 4.0; en-US; rv:1.9.1.20) Gecko/2015-07-27 10:27:01 Firefox/5.0" 2247'
-    print re.match(matcher, line).group(group)
+    matches = re.match(log_re, line)
+    # import code; code.interact(local=dict(globals(), **locals()))
+    print matches.group(match_group)
 
 def main():
     if len(sys.argv) < 2:
@@ -60,7 +61,16 @@ def main():
     filename = sys.argv[1]
     # parse(filename)
 
-    run_matcher(matchers['ip'], 'ip')
+    run_matcher('ip')
+    run_matcher('identd')
+    run_matcher('userid')
+    run_matcher('timestamp')
+    run_matcher('uri')
+    run_matcher('status_code')
+    run_matcher('size')
+    run_matcher('referer')
+    run_matcher('user_agent')
+    run_matcher('duration_microseconds')
 
 if __name__ == '__main__':
     main()
